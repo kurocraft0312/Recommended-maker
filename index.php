e165626aaa0cf84592bffb9512cf0ea5e2a6654c
kurocraft0312 <kurocraft0312@gmail.com>
2019-06-01 12:11:01 +0900

    リセットCSSとCSS・JSファイルの外部読み込みを設定しました。

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="style.css">
 	<title>Recommended-marker</title>
 </head>
 <body>
-	
+	<script src="js/script.js"></script>
 </body>
 </html>
